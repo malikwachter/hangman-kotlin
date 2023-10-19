18938e3835c71e73d9b5ce7ba4922be9de8d3797
Malik Wachter <malik@pauls-air-2.ovm.intern>
2023-10-19 11:09:59 +0200

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main() {
+    println("Welcome to hangman! Choose a gamemode:\n 1) Singleplayer (A random word gets choosen by the computer)\n 2) Multiplayer (Another player can choose a word.)")
+
+}
